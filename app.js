59c61b6e7aa68c8efd65d6d33255433624de69b1
Justin Z <contactjustinzamora@gmail.com>
2021-01-11 00:14:14 -0800

    added error handling class

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+
+//MIDDLEWARE
+app.use(morgan('dev'))
+app.use(express.json())
+
+//ROUTES
 app.get('/', (req, res, next)=>{
     return res.json('helloworld')
 })
 
+//GENERAL ERROR HANDLING
 module.exports = app
