98aea549ae284eecf7d02af58b17d0c877fcebd6
Justin Z <contactjustinzamora@gmail.com>
2021-01-12 00:07:53 -0800

    added error handling on app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const ExpressError = require('../express-side-project/expressError')
 
 const app = express()
 
@@ -13,5 +14,15 @@ app.get('/', (req, res, next)=>{
     return res.json('helloworld')
 })
 
+//404 ERROR MISSING PAGES
+app.use((req,res,next)=>{
+    const e = ExpressError('Page Not Found', 404)
+})
+
 //GENERAL ERROR HANDLING
+app.use((err, req,res, next)=>{
+    const status = err.status || 500
+    const message = err.message
+    return res.status(status).json({Error:{message, status}})
+})
 module.exports = app
